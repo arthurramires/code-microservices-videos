cb543fd4d016e70983e48b2b2f2404f039505f86
arthurramires <arthurramires1802@outlook.com>
2020-09-24 09:23:12 -0400

    Organizando asserções na edição

--- tests/Feature/Http/Controllers/Api/CategoryControllerTest.php
+++ tests/Feature/Http/Controllers/Api/CategoryControllerTest.php
@@ -90,22 +90,32 @@ class CategoryControllerTest extends TestCase
     }
 
     public function testUpdate(){
-        $category = factory(Category::class)->create([
+        $this->category = factory(Category::class)->create([
             'is_active' => false
         ]);
-        $response = $this->json('PUT', route('categories.update', ['category' => $category->id]),
-        [
+
+        $data = [
             'name' => 'test',
             'is_active' => true,
             'description' => 'test'
+        ];
+        $response = $this->assertUpdate($data, $data + ['deleted_at' => null]);
+        $response->assertJsonStructure([
+            'created_at', 
+            'updated_at'
         ]);
 
-        $id = $response->json('id');
-        $category = Category::find($id);
+        $data = [
+            'name' => 'test',
+            'description' => ''
+        ];
+        $this->assertUpdate($data, array_merge($data, ['description' => null]));
 
-        $response
-            ->assertStatus(200)
-            ->assertJson($category->toArray());
+        $data['description'] = 'test';
+        $this->assertUpdate($data, array_merge($data, ['description' => 'test']));
+
+        $data['description'] = null;
+        $this->assertUpdate($data, array_merge($data, ['description' => null]));
     }
 
     public function testDestroy(){
