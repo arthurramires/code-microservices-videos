756b7f52ed2c5bc0144be4223e978200814428db
arthurramires <arthurramires1802@outlook.com>
2020-10-06 09:05:20 -0400

    Criando testando métodos para excluir arquivos

--- app/Models/Traits/UploadFiles.php
+++ app/Models/Traits/UploadFiles.php
@@ -19,4 +19,18 @@ trait UploadFiles
     public function uploadFile(UploadedFile $file){
         $file->store($this->uploadDir());
     }
+
+    public function deleteFiles(array $files){
+        foreach($files as $file){
+            $this->deleteFile($file);
+        }
+    }
+
+    /** 
+     * @param string|UploadedFile $file
+    */
+    public function deleteFile($file){
+        $filename = $file instanceof UploadedFile ? $file->hashName() : $file;
+        \Storage::delete("{$this->uploadDir()}/{$filename}");
+    }
 }
