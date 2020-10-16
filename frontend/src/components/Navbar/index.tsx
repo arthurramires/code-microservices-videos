955e8478013c8404ddfd6132ae4b12ce5f3a4fff
arthurramires <arthurramires1802@outlook.com>
2020-10-16 09:02:41 -0400

    Iniciando com temas e makeStyles

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -1,16 +1,22 @@
 import * as React from 'react';
-import {AppBar, Toolbar,Typography, Button, makeStyles} from '@material-ui/core';
+import {AppBar, Toolbar,Typography, Button, makeStyles, Theme} from '@material-ui/core';
 import logo from '../../static/img/logo.png';
 
-const useStyles = makeStyles({
+const useStyles = makeStyles((theme: Theme) => ({
     toolbar: {
         backgroundColor: '#000000',
     },
     title: {
         flexGrow: 1,
         textAlign: 'center'
+    }, 
+    logo: {
+        width: 100,
+        [theme.breakpoints.up('sm')]: {
+            widht: 170
+        }
     }
-});
+}));
 
 export const Navbar: React.FC = () => {
     const classes = useStyles();
