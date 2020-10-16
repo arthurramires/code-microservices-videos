a89dfaaf26c79e163675f0e5d72541a7bda79e8f
arthurramires <arthurramires1802@outlook.com>
2020-10-16 08:25:20 -0400

    Criando primeiro componente com Material UI - Navbar

---
+++ frontend/src/components/Navbar/index.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import {AppBar, Toolbar} from '@material-ui/core';
+// import { Container } from './styles';
+
+export const Navbar: React.FC = () => {
+  return (
+    <AppBar>
+        <Toolbar>
+            Texto
+        </Toolbar>
+    </AppBar>
+  );    
+}
+
