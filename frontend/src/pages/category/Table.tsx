1cdeecfa66c367e5b8e085710400657da5864f08
arthurramires <arthurramires1802@outlook.com>
2020-10-19 09:33:38 -0400

    Configurando Axios

--- frontend/src/pages/category/Table.tsx
+++ frontend/src/pages/category/Table.tsx
@@ -1,10 +1,35 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import MUIDataTable, { MUIDataTableColumn } from 'mui-datatables';
+import {httpVideo} from '../../utils/http';
+const columnDefinitions: MUIDataTableColumn[] =[
+    {
+        name: "name",
+        label: "Nome"
+    },
+    {
+        name: "is_active",
+        label: "Ativo?"
+    },
+    {
+        name: "created_at",
+        label: "Criado em:"
+    },
+];
 
 const Table: React.FC = () => {
+    const [categories, setCategories] = useState([]);
+
+    useEffect(() => {
+        httpVideo.get('categories').then(
+            response => setCategories(response.data.data)
+        )
+    }, []);
   return (
-      <div>
-          Conteudo
-      </div>
+      <MUIDataTable
+        title="Listagem de categorias" 
+        columns={columnDefinitions}
+        data={categories}
+    />
   );
 }
 
