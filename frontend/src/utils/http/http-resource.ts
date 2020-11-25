5480ae97f8d63340f5ae3bc563625d8f3ce0e0c4
arthurramires <arthurramires1802@outlook.com>
2020-11-25 11:56:35 -0400

    Implementando query Params no http-resource

--- frontend/src/utils/http/http-resource.ts
+++ frontend/src/utils/http/http-resource.ts
@@ -1,10 +1,14 @@
-import { AxiosInstance, AxiosResponse } from "axios";
+import { AxiosInstance, AxiosResponse, AxiosRequestConfig } from "axios";
 
 export default class httpResource {
 
     constructor(protected http: AxiosInstance, protected resource){}
 
-    list<T = any>(): Promise<AxiosResponse<T>>{
+    list<T = any>(options?: { queryParams? }): Promise<AxiosResponse<T>>{
+        const config: AxiosRequestConfig = {};
+        if(options && options.queryParams){
+            config.params = options.queryParams;
+        }
         return this.http.get<T>(this.resource);
     }
 
